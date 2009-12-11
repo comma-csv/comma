355f21592eb2df1913b76bdc81f6a210bdd3b261
Stefan Penner <stefan.penner@gmail.com>
2009-12-11 16:48:18 -0600

    Support rails versions older then 2.3.5, which still require 'activesupport', Fall back on ruby's built in csv library if fastercsv is not present or if its ruby 1.9.x

--- lib/comma.rb
+++ lib/comma.rb
@@ -1,12 +1,22 @@
-require 'active_support'
-require 'comma/extractors'
-
-if RUBY_VERSION =~ /^1.9/
-  require 'csv'
-  FasterCSV = CSV
-else
-  require 'fastercsv'
+if defined? Rails and Rails.version < '2.3.5'
+  require 'activesupport' 
+else 
+  require 'active_support'
 end
+
+if RUBY_VERSION >= '1.9'
+  require 'csv' 
+else 
+  begin
+    # try faster csv
+    require 'fastercsv'
+  rescue Error => e
+    Rails.logger.info "FasterCSV not installed, falling back on CSV"
+    require 'csv'
+  end
+end
+
+require 'comma/extractors'
 
 class Array
   def to_comma(style = :default)
