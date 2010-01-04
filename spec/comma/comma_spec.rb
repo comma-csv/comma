e03aebbc41eb9dda37f9907853711fbcea0e8ad8
Benjamin Quorning <benjamin@quorning.net>
2010-01-04 21:17:41 +0100

    Removed trailing spaces

--- spec/comma/comma_spec.rb
+++ spec/comma/comma_spec.rb
@@ -1,31 +1,31 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Comma do
-  
+
   it 'should extend object to add a comma method' do
     Object.should respond_to(:comma)
   end
-  
+
   it 'should extend object to have a to_comma method' do
     Object.should respond_to(:to_comma)
   end
-  
+
   it 'should extend object to have a to_comma_headers method' do
     Object.should respond_to(:to_comma_headers)
   end
-  
+
 end
 
 describe Comma, 'generating CSV' do
-  
+
   before do
     @isbn = Isbn.new('123123123', '321321321')
     @book = Book.new('Smalltalk-80', 'Language and Implementation', @isbn)
-    
+
     @books = []
     @books << @book
   end
-  
+
   it 'should extend Array to add a #to_comma method which will return CSV content for objects within the array' do
     @books.to_comma.should == "Title,Description,Issuer,ISBN-10,ISBN-13\nSmalltalk-80,Language and Implementation,ISBN,123123123,321321321\n"
   end
@@ -69,30 +69,30 @@ describe Comma, 'generating CSV' do
 end
 
 describe Comma, 'defining CSV descriptions' do
-  
+
   describe 'with an unnamed description' do
-    
+
     before do
       class Foo
         comma do; end
-      end        
+      end
     end
-  
+
     it 'should name the current description :default if no name has been provided' do
       Foo.comma_formats.should_not be_empty
       Foo.comma_formats[:default].should_not be_nil
     end
   end
-  
+
   describe 'with a named description' do
-   
+
     before do
       class Bar
         comma do; end
         comma :detailed do; end
-      end        
+      end
     end
-    
+
     it 'should use the provided name to index the comma format' do
       Bar.comma_formats.should_not be_empty
       Bar.comma_formats[:default].should_not be_nil
@@ -102,22 +102,22 @@ describe Comma, 'defining CSV descriptions' do
 end
 
 describe Comma, 'to_comma data/headers object extensions' do
-  
+
   describe 'with unnamed descriptions' do
-  
+
     before do
       class Foo
         attr_accessor :content
         comma do; content; end
-      
+
         def initialize(content)
           @content = content
         end
       end
-    
+
       @foo = Foo.new('content')
     end
-  
+
     it 'should return and array of data content, using the :default CSV description if none requested' do
       @foo.to_comma.should == %w(content)
     end
@@ -125,28 +125,28 @@ describe Comma, 'to_comma data/headers object extensions' do
     it 'should return and array of header content, using the :default CSV description if none requested' do
       @foo.to_comma_headers.should == %w(Content)
     end
-    
+
     it 'should return the CSV representation including header and content when called on an array' do
       Array(@foo).to_comma.should == "Content\ncontent\n"
     end
 
   end
-  
+
   describe 'with named descriptions' do
-    
+
     before do
       class Foo
         attr_accessor :content
         comma :detailed do; content; end
-      
+
         def initialize(content)
           @content = content
         end
       end
-    
+
       @foo = Foo.new('content')
     end
-  
+
     it 'should return and array of data content, using the :default CSV description if none requested' do
       @foo.to_comma(:detailed).should == %w(content)
     end
@@ -154,17 +154,17 @@ describe Comma, 'to_comma data/headers object extensions' do
     it 'should return and array of header content, using the :default CSV description if none requested' do
       @foo.to_comma_headers(:detailed).should == %w(Content)
     end
-    
+
     it 'should return the CSV representation including header and content when called on an array' do
       Array(@foo).to_comma(:detailed).should == "Content\ncontent\n"
     end
-    
+
     it 'should raise an error if the requested description is not avaliable' do
       lambda { @foo.to_comma(:bad) }.should raise_error
       lambda { @foo.to_comma_headers(:bad) }.should raise_error
       lambda { Array(@foo).to_comma(:bad) }.should raise_error
     end
-    
+
   end
 
   describe 'with block' do
@@ -178,33 +178,33 @@ describe Comma, 'to_comma data/headers object extensions' do
           created_at &time_to_s
           updated_at &time_to_s
         end
-        
+
         def initialize(content, created_at = Time.now, updated_at = Time.now)
           @content = content
           @created_at = created_at
           @updated_at = updated_at
         end
       end
-      
+
       @time = Time.now
       @content = 'content ' * 5
       @foo = Foo.new @content, @time, @time
     end
-    
+
     it 'should return yielded values by block' do
       header, foo = Array(@foo).to_comma.split("\n")
       foo.should == [@content, @content[0..10], @time.to_s(:db), @time.to_s(:db)].join(',')
     end
-    
+
   end
-  
+
   describe 'on an object with no comma declaration' do
-    
+
     it 'should raise an error mentioning there is no comma description defined for that class' do
       lambda { 'a string'.to_comma }.should raise_error('No comma format for class String defined for style default')
       lambda { 'a string'.to_comma_headers }.should raise_error('No comma format for class String defined for style default')
     end
-    
+
   end
-  
+
 end
--- spec/comma/extractors_spec.rb
+++ spec/comma/extractors_spec.rb
@@ -3,89 +3,89 @@ require File.dirname(__FILE__) + '/../spec_helper'
 # comma do
 #   name 'Title'
 #   description
-#   
+#
 #   isbn :number_10 => 'ISBN-10', :number_13 => 'ISBN-13'
 # end
 
 describe Comma::HeaderExtractor do
-  
+
   before do
     @isbn = Isbn.new('123123123', '321321321')
     @book = Book.new('Smalltalk-80', 'Language and Implementation', @isbn)
-    
+
     @headers = @book.to_comma_headers
   end
-  
+
   describe 'when no parameters are provided' do
-    
+
     it 'should use the method name as the header name, humanized' do
       @headers.should include('Description')
     end
   end
-  
+
   describe 'when given a string description as a parameter' do
-    
+
     it 'should use the string value, unmodified' do
       @headers.should include('Title')
     end
   end
-  
+
   describe 'when an hash is passed as a parameter' do
-    
+
     describe 'with a string value' do
-      
+
       it 'should use the string value, unmodified' do
         @headers.should include('ISBN-10')
       end
     end
-    
+
     describe 'with a non-string value' do
-      
+
       it 'should use the non string value converted to a string, humanized' do
         @headers.should include('Issuer')
       end
     end
-    
+
   end
-    
+
 end
 
 describe Comma::DataExtractor do
-  
+
   before do
     @isbn = Isbn.new('123123123', '321321321')
     @book = Book.new('Smalltalk-80', 'Language and Implementation', @isbn)
-    
+
     @data = @book.to_comma
   end
-  
+
   describe 'when no parameters are provided' do
-    
+
     it 'should use the string value returned by sending the method name on the object' do
       @data.should include('Language and Implementation')
     end
   end
-  
+
   describe 'when given a string description as a parameter' do
-    
+
     it 'should use the string value returned by sending the method name on the object' do
       @data.should include('Smalltalk-80')
     end
   end
-  
+
   describe 'when an hash is passed as a parameter' do
-    
+
     describe 'with a string value' do
-      
+
       it 'should use the string value, returned by sending the hash key to the object' do
         @data.should include('123123123')
         @data.should include('321321321')
       end
-      
+
       it 'should not fail when an associated object is nil' do
         lambda { Book.new('Smalltalk-80', 'Language and Implementation', nil).to_comma }.should_not raise_error
       end
     end
   end
-  
+
 end
