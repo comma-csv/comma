fa400368eb30d5d908b1bb28feb78477de65a0ba
Tom Meier <ozmeier@yahoo.co.uk>
2010-04-23 11:33:59 +1000

    Stream csv content to handle large files

--- lib/comma/render_as_csv.rb
+++ lib/comma/render_as_csv.rb
@@ -5,8 +5,36 @@ module RenderAsCSV
 
   def render_with_csv(options = nil, extra_options = {}, &block)
     return render_without_csv(options, extra_options, &block) unless options.is_a?(Hash) and options[:csv].present?
-    data = options.delete(:csv)
-    style = options.delete(:style) || :default
-    send_data Array(data).to_comma(style), options.merge(:type => :csv)
+
+    content  = options.delete(:csv)
+    style    = options.delete(:style) || :default
+    filename = options.delete(:filename)
+
+    headers.merge!(
+      'Content-Transfer-Encoding' => 'binary',
+      'Content-Type'              => 'text/csv'
+    )
+    headers.merge!('Content-Disposition' => "attachment; filename=\"#{filename}\"") if options.delete(:filename).present?
+
+    @performed_render = false
+
+    render_stream :status => 200,
+                  :content => Array(content),
+                  :style => style
+  end
+
+  protected
+
+  def render_stream(options)
+    status  = options[:status]
+    content = options[:content]
+    style   = options[:style]
+
+    render :status => status, :text => Proc.new { |response, output|
+      output.write FasterCSV.generate_line(content.first.to_comma_headers(style))
+      content.each { |line| output.write FasterCSV.generate_line(line.to_comma(style)) }
+    }
   end
 end
+
+#credit : http://ramblingsonrails.com/download-a-large-amount-of-data-in-csv-from-rails
