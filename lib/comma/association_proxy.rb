3fa28624ba59d90c04973cddaea5274c86a8cefc
Tom Meier <ozmeier@yahoo.co.uk>
2010-05-11 16:07:00 +1000

    Fix association proxy rails bug with an array workaround

--- lib/comma/association_proxy.rb
+++ lib/comma/association_proxy.rb
@@ -1,5 +1,6 @@
 class ActiveRecord::Associations::AssociationProxy
   def to_comma(style = :default)
-    Comma::Generator.new(self, style).run(:each)
+    #Bug in Rails 2.3.5, this is a workaround as association_proxy.rb doesn't pass the &block in the send method so it silently fails
+    Comma::Generator.new(Array(self), style).run(:each)
   end
 end
