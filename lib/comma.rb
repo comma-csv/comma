669beaac24f90a68667378145e78701bef630a6b
jonathanb <jxb6065@rit.edu>
2011-03-18 01:00:22 -0400

    added a require statement needed to use the blank? method

--- lib/comma.rb
+++ lib/comma.rb
@@ -3,6 +3,7 @@ if defined? Rails and (Rails.version.split('.').map(&:to_i) <=> [2,3,5]) < 0
   require 'activesupport'
 else
   require 'active_support/core_ext/class/inheritable_attributes'
+  require 'active_support/core_ext/object/'
 end
 
 # load the right csv library
