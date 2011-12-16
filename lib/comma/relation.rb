db98a73c8e7590af671d3b10afbfca2f0609e7ce
Tom Meier <tom@venombytes.com>
2011-12-16 23:14:26 +1100

    Use find_each and not on the collection for a relation

--- lib/comma/relation.rb
+++ lib/comma/relation.rb
@@ -1,5 +1,5 @@
 class ActiveRecord::Relation
   def to_comma(style = :default)
-    Comma::Generator.new(self.all, style).run(:find_each)
+    Comma::Generator.new(self, style).run(:find_each)
   end
 end
