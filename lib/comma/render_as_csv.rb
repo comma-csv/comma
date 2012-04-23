34885f9e1dc24e94ff9dc18a6cea8086dc4116ad
James Rissler <risslerjames@gmail.com>
2012-04-23 17:38:48 -0300

    Updated for Rails 2.3.14 (latest stable branch)

--- lib/comma/render_as_csv.rb
+++ lib/comma/render_as_csv.rb
@@ -1,49 +1,50 @@
-module RenderAsCSV
-  def self.included(base)
-    base.alias_method_chain :render, :csv
+if defined?(ActionController::Renderers) && ActionController::Renderers.respond_to?(:add)
+  ActionController::Renderers.add :csv do |obj, options|
+    filename = options[:filename] || 'data'
+    send_data obj.to_comma, :type => Mime::CSV, :disposition => "attachment; filename=#{filename}.csv"
   end
+else
+  module RenderAsCSV
+    def self.included(base)
+      base.alias_method_chain :render, :csv
+    end
 
-  def render_with_csv(options = nil, extra_options = {}, &block)
-    return render_without_csv(options, extra_options, &block) unless options.is_a?(Hash) and options[:csv].present?
+    def render_with_csv(options = nil, extra_options = {}, &block)
+      return render_without_csv(options, extra_options, &block) unless options.is_a?(Hash) and options[:csv].present?
 
-    content  = options.delete(:csv)
-    style    = options.delete(:style) || :default
-    filename = options.delete(:filename)
+      content  = options.delete(:csv)
+      style    = options.delete(:style) || :default
+      filename = options.delete(:filename)
 
-    headers.merge!(
-      'Content-Transfer-Encoding' => 'binary',
-      'Content-Type'              => 'text/csv; charset=utf-8'
-    )
-    filename_header_value = "attachment"
-    filename_header_value += "; filename=\"#{filename}\"" if filename.present?
-    headers.merge!('Content-Disposition' => filename_header_value)
+      headers.merge!(
+        'Content-Transfer-Encoding' => 'binary',
+        'Content-Type'              => 'text/csv; charset=utf-8'
+      )
+      filename_header_value = "attachment"
+      filename_header_value += "; filename=\"#{filename}\"" if filename.present?
+      headers.merge!('Content-Disposition' => filename_header_value)
 
-    @performed_render = false
+      @performed_render = false
 
-    render_stream :status => 200,
-                  :content => Array(content),
-                  :style => style
-  end
+      render_stream :status => 200,
+                    :content => Array(content),
+                    :style => style
+    end
 
-  protected
+    protected
 
-  def render_stream(options)
-    status  = options[:status]
-    content = options[:content]
-    style   = options[:style]
+    def render_stream(options)
+      status  = options[:status]
+      content = options[:content]
+      style   = options[:style]
 
-    if defined? Rails and (Rails.version.split('.').map(&:to_i) <=> [2,3,5]) < 0
+      #rails 2 specific
       render :status => status, :text => Proc.new { |response, output|
-        output.write CSV_HANDLER.generate_line(content.first.to_comma_headers(style))
-        content.each { |line| output.write CSV_HANDLER.generate_line(line.to_comma(style)) }
+        output.write FasterCSV.generate_line(content.first.to_comma_headers(style))
+        content.each { |line| output.write FasterCSV.generate_line(line.to_comma(style)) }
       }
-    else
-      self.status = status
-      self.response_body = proc { |response, output|
-        output.write CSV_HANDLER.generate_line(content.first.to_comma_headers(style))
-        content.each { |line| output.write CSV_HANDLER.generate_line(line.to_comma(style)) }
-      }
+
     end
   end
+  #credit : http://ramblingsonrails.com/download-a-large-amount-of-data-in-csv-from-rails
 end
-#credit : http://ramblingsonrails.com/download-a-large-amount-of-data-in-csv-from-rails
