28daf5f4c1aac7150af44cd6ccd4a04d42361271
Eito Katagiri <eitoball@gmail.com>
2015-04-05 03:24:39 +0900

    fix #89

--- lib/comma/object.rb
+++ lib/comma/object.rb
@@ -3,10 +3,20 @@ require 'comma/data_extractor'
 require 'comma/header_extractor'
 
 class Object
-  class_attribute :comma_formats
+  class << self
+    def comma(style = :default, &block)
+      (@comma_formats ||= {})[style] = block
+    end
 
-  def self.comma(style = :default, &block)
-    (self.comma_formats ||= {})[style] = block
+    def comma_formats
+      @comma_formats ||= begin
+        if self == Object
+          {}
+        else
+          self.superclass.comma_formats
+        end
+      end
+    end
   end
 
   def to_comma(style = :default)
@@ -21,10 +31,12 @@ class Object
 
   def extract_with(extractor_class, style = :default)
     raise_unless_style_exists(style)
-    extractor_class.new(self, style, self.comma_formats).results
+    formats = respond_to?(:comma_formats) ? self.comma_formats : self.class.comma_formats
+    extractor_class.new(self, style, formats).results
   end
 
   def raise_unless_style_exists(style)
-    raise "No comma format for class #{self.class} defined for style #{style}" unless self.comma_formats && self.comma_formats[style]
+    formats = respond_to?(:comma_formats) ? self.comma_formats : self.class.comma_formats
+    raise "No comma format for class #{self.class} defined for style #{style}" unless formats && formats[style]
   end
 end
