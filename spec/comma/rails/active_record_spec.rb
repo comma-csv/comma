comma-csv/comma

commit 28daf5f4c1aac7150af44cd6ccd4a04d42361271

fix #89

lib/comma/object.rb
@@ -3,10 +3,20 @@ require 'comma/data_extractor'
 require 'comma/header_extractor'
 
 class Object
-  class_attribute :comma_formats
+  class << self
+    def comma(style = :default, &block)
+      (@comma_formats ||= {})[style] = block
+    end
 
-  def self.comma(style = :default, &block)
-    (self.comma_formats ||= {})[style] = block
+    def comma_formats
+      @comma_formats ||= begin
+        if self == Object
+          {}
+        else
+          self.superclass.comma_formats
+        end
+      end
+    end
   end
 
   def to_comma(style = :default)
@@ -21,10 +31,12 @@ class Object
 
   def extract_with(extractor_class, style = :default)
     raise_unless_style_exists(style)
-    extractor_class.new(self, style, self.comma_formats).results
+    formats = respond_to?(:comma_formats) ? self.comma_formats : self.class.comma_formats
+    extractor_class.new(self, style, formats).results
   end
 
   def raise_unless_style_exists(style)
-    raise "No comma format for class #{self.class} defined for style #{style}" unless self.comma_formats && self.comma_formats[style]
+    formats = respond_to?(:comma_formats) ? self.comma_formats : self.class.comma_formats
+    raise "No comma format for class #{self.class} defined for style #{style}" unless formats && formats[style]
   end
 end

commit e8bc9f7bc8c6b60eb550fe126b212e5ea6a7cd06

fix regression caused by 28daf5f4c1aac7150af44cd6ccd4a04d42361271
The regression is reported at https://github.com/comma-csv/comma/issues/89#issuecomment-90549440

lib/comma/object.rb
@@ -3,19 +3,16 @@ require 'comma/data_extractor'
 require 'comma/header_extractor'
 
 class Object
+  class_attribute :comma_formats
+
   class << self
     def comma(style = :default, &block)
-      (@comma_formats ||= {})[style] = block
+      (self.comma_formats ||= {})[style] = block
     end
 
-    def comma_formats
-      @comma_formats ||= begin
-        if self == Object
-          {}
-        else
-          self.superclass.comma_formats
-        end
-      end
+    def inherited(subclass)
+      super
+      subclass.comma_formats = self.comma_formats ? self.comma_formats.dup : {}
     end
   end
 
@@ -31,12 +28,10 @@ class Object
 
   def extract_with(extractor_class, style = :default)
     raise_unless_style_exists(style)
-    formats = respond_to?(:comma_formats) ? self.comma_formats : self.class.comma_formats
-    extractor_class.new(self, style, formats).results
+    extractor_class.new(self, style, self.comma_formats).results
   end
 
   def raise_unless_style_exists(style)
-    formats = respond_to?(:comma_formats) ? self.comma_formats : self.class.comma_formats
-    raise "No comma format for class #{self.class} defined for style #{style}" unless formats && formats[style]
+    raise "No comma format for class #{self.class} defined for style #{style}" unless self.comma_formats && self.comma_formats[style]
   end
 end

spec/comma/comma_spec.rb
@@ -16,7 +16,8 @@ describe Comma do
 
   describe '.to_comma_header' do
     it 'should not crash (#94)' do
-      klass = Class.new do
+      klass = Class.new
+      klass.instance_eval do
         attr_accessor :name
 
         comma :brief do

spec/comma/rails/active_record_spec.rb
@@ -150,12 +150,16 @@ if defined? ActiveRecord
   end
 
   describe Comma, 'generating CSV from an ActiveRecord object using Single Table Inheritance' do
-
     class Animal < ActiveRecord::Base
-      comma do;
+      comma do
         name 'Name' do |name|
           'Super-' + name
         end
+      end
+
+      comma :with_type do
+        name
+        type
       end
     end
 
@@ -192,6 +196,8 @@ if defined? ActiveRecord
       @cat.to_comma.should == %w(Super-Kitty)
     end
 
+    it 'should call definion in parent class' do
+      lambda { @dog.to_comma(:with_type) }.should_not raise_error
+    end
   end
-
 end